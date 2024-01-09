882fd29e9e54a65656e687aba58b8437050a79d3
Nuestra-Boda-Luis-Y-Fer <its.alveg@gmail.com>
2024-01-08 21:44:19 -0600

    Se modificaron varios archivos

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// SCRIPT PARA MANDAR WSP AL NOVIO 
+
+function enviarMensaje() {
+    // Número de teléfono al que se enviará el mensaje (agrega el código de país)
+    var numeroTelefono = '+522481629018';
+
+    // Mensaje que se enviará
+    var mensaje = 'Hola, quiero confirmar mi asistencia';
+
+    // Construir el enlace de WhatsApp
+    var enlaceWhatsApp = 'https://wa.me/' + numeroTelefono + '?text=' + encodeURIComponent(mensaje);
+
+    // Abrir el enlace en una nueva ventana o pestaña
+    window.open(enlaceWhatsApp, '_blank');
+}
+
+// SCRIPT PARA MANDAR WSP A LA NOVIA
+function enviarMensaje2() {
+    // Número de teléfono al que se enviará el mensaje (agrega el código de país)
+    var numeroTelefono = '+522481255393';
+
+    // Mensaje que se enviará
+    var mensaje = 'Hola, quiero confirmar mi asistencia';
+
+    // Construir el enlace de WhatsApp
+    var enlaceWhatsApp = 'https://wa.me/' + numeroTelefono + '?text=' + encodeURIComponent(mensaje);
+
+    // Abrir el enlace en una nueva ventana o pestaña
+    window.open(enlaceWhatsApp, '_blank');
+}
