7d87ebbb5caf137c9c5fe3e06ff36faa9697e3cc
Nuestra-Boda-Luis-Y-Fer <its.alveg@gmail.com>
2024-01-12 13:43:19 -0600

    Cambios en la música, estilos, se agregaron botones para fb, instagram, pausar la música, se cambió el script de la música para poder pausarla, y se corrigieron errores menores.

--- js/musica.js
+++ js/musica.js
@@ -1,13 +1,17 @@
-// SCRIPT PARA LA REPRODUCCION AUTOMATICA
 var reproductor = document.getElementById('reproductor');
-function toggleMute() {
-    if (reproductor.muted) {
-        reproductor.muted = false;
+var pauseIcon = document.getElementById('pause-icon');
+
+function togglePlayPause() {
+    if (reproductor.paused) {
+        reproductor.play();
+        pauseIcon.innerHTML = '<i style="font-size: 30px;" class="bx bx-pause-circle"></i>';
     } else {
-        reproductor.muted = true;
+        reproductor.pause();
+        pauseIcon.innerHTML = '<i style="font-size: 30px;" class="bx bx-play-circle"></i>';
     }
 }
 
+// Reproducir automáticamente al hacer clic en cualquier parte de la página
 document.addEventListener("click", function () {
     reproductor.play();
     document.removeEventListener("click", arguments.callee);
